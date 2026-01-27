cf5008dfd72e2b508858866076989a394242075a
Ingo Stramm <ingostramm@gmail.com>
2026-01-27 10:48:11 -0300

    add archive obras

--- archive-projetos.php
+++ archive-projetos.php
@@ -11,15 +11,35 @@
 get_header(); ?>
 
 <?php
-get_template_part('template-parts/general/help', 'section', array('title' => __('Projetos', 'pu'), 'text' => __('Os projetos ajudam a planejar a obra.', 'pu'), 'url' => '#'));
-get_template_part('template-parts/general/new-projeto', 'section');
+$status = isset($_GET['status']) && ! empty($_GET['status']) ? $_GET['status'] : 'projeto';
+
+$help_section_title = $status === 'projeto' ? __('Projetos', 'pu') : __('Obras', 'pu');
+$help_section_text = $status === 'projeto' ? __('Os projetos ajudam a planejar a obra', 'pu') : __('Acompanhamento completo da obra e transparência com cliente.', 'pu');
+$help_section_url = '#';
+
+get_template_part(
+    'template-parts/general/help',
+    'section',
+    array(
+        'title' => $help_section_title,
+        'text' => $help_section_text,
+        'url' => $help_section_url
+    )
+);
+if ($status === 'projeto') {
+    get_template_part('template-parts/general/new-projeto', 'section');
+}
 if (have_posts()) { ?>
     <div class="row gap-4">
         <?php
         // Load posts loop.
         while (have_posts()) {
             the_post();
-            get_template_part('template-parts/archive/archive-projetos', 'content');
+            if ($status === 'projeto') {
+                get_template_part('template-parts/archive/archive-projetos', 'content');
+            } else {
+                get_template_part('template-parts/archive/archive-obras', 'content');
+            }
         } ?>
         <?php pu_paging_nav(); ?>
     </div>
---
+++ template-parts/archive/archive-obras-content.php
@@ -0,0 +1,3 @@
+<div class="col-md-12">
+    <?php get_template_part('template-parts/general/obra', 'item'); ?>
+</div>
